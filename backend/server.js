dd37c97ddae30e2893213bfc449788c8f84291be
Madison Hoyt <mhoyt@HDMs-MacBook-Air-2.local>
2026-09-23 15:07:02 -0600

    Initial REST API for goals

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,17 @@
 const express = require("express");
+const colors = require("colors");
 const dotenv = require("dotenv").config(); //allows us to use .env
-const {errorHandler} = require('./middleware/errorMiddleware')
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
 
+connectDB();
 const app = express();
 
 //These two lines are needed to read json.body
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/goals', require('./routes/goalRoutes'))
-app.use(errorHandler)
+app.use("/api/goals", require("./routes/goalRoutes"));
+app.use(errorHandler);
 app.listen(port, () => console.log(`Server started on port ${port}`));
